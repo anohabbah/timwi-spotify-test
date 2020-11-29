5d011e210dca050532d65a0e350e60597b507f28
Abbah <anohabbah@gmail.com>
2020-11-29 13:33:34 +0100

    feat(library): setup library page shell

--- pages/library.vue
+++ pages/library.vue
@@ -1,1 +1,21 @@
-<!--<template> </template>-->
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col md="3">
+        <v-card>
+          <v-img />
+          <v-card-title></v-card-title>
+          <v-card-subtitle></v-card-subtitle>
+          <v-divider class="mx-4" />
+          <v-card-actions>
+            <v-row align="center" justify="center" no-gutters>
+              <span class="subheading"> 2 morceaux </span>
+              <span class="mx-2">&bullet;</span>
+              <span class="subheading">sortie le Dec,16, 2020</span>
+            </v-row>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
